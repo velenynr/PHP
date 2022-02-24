27e804809715ce3fef12e72a3a187fafe97eea10
Velen 吳坪紋 <100340902+velenynr@users.noreply.github.com>
2022-02-24 19:35:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+<?php
+
+echo "<p align = right> Today's date is : ";
+echo date ("Y/m/d");
+echo "</p><h1>Welcome!</h1>";
+
+echo "Let's do simple calculation<br>";
+$num1 = 3;
+$num2 = 5;
+echo "$num1 multiply $num2 equals ";
+echo $num1*$num2;
+
+echo "<br><br>It's my first PHP program.<br>";
+
+echo "<br>It all seems plain, but..";
+echo "<p align = right>...after I learn more, believe me,</p>";
+echo "<h3><p align = center>This page will be AWESOME</p></h3>";
+?>
+
+
+</body>
+
+</html>
